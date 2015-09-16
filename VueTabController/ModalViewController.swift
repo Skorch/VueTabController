4d39000c7f69d8c6b833535f99c7032b071b630d
Drew Beaupre <drew@beauprefamily.org>
2015-09-16 13:52:17 -0700

    adding close button to modal view controller

---
+++ VueTabController/ModalViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ModalViewController.swift
+//  VueTabController
+//
+//  Created by Drew Beaupre on 2015-09-16.
+//  Copyright © 2015 drew.beaupre. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeButtonTapped(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
